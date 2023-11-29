c5375cea8ab55805e4e7a730123f0f34684601df
TANG Ho Tung <hotung1027@gmail.com>
2023-11-29 17:24:00 +0800

    fix : Vista now use neovim LSP provider by default

--- .nvim/lua/config/tags.lua
+++ .nvim/lua/config/tags.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
 let g:vista_icon_indent = ["╰─▸ ", "├─▸ "]
 ]])
-vim.g.vista_default_executive = "ctags"
+vim.g.vista_default_executive = "nvim_lsp"
 vim.g.vista_ctags_executable = "ctags"
 
 
@@ -24,8 +24,7 @@ vim.cmd([[let g:vista#renderer#enable_kind = 1]])
 vim.g.vista_update_on_text_changed = 1
 vim.cmd([[
   let g:vista#renderer#icons ={
-  \ "function" : "",
-  \ "variable" : "",
+  \ "function" : "\uf794",
+  \ "variable" : "\uf71b",
   \ }
 ]])
-
