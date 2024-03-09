3301634f08fad287074ea79f67576fff707a2c91
TANG Ho Tung <hotung1027@gmail.com>
2024-03-10 01:47:20 +0800

    new-plugin : obsidian.nvim

--- .nvim/lua/plugins.lua
+++ .nvim/lua/plugins.lua
@@ -711,7 +711,7 @@ return require('lazy').setup(
       'Civitasv/cmake-tools.nvim',
       dependencies = { 'stevearc/overseer.nvim' },
       opts = {},
-      version = 'b7554dd2aeb436'
+      commit = 'aba5b805082b3c1027ac4f5051b84c61989c34c8'
     },
     { 'rhysd/vim-clang-format',     ft = { 'cpp', 'c', 'h', 'hpp' }, },
     { 'p00f/clangd_extensions.nvim' },
@@ -886,8 +886,46 @@ return require('lazy').setup(
 
     -- Faster footnote generation
     { "vim-pandoc/vim-markdownfootnotes", ft = { "markdown" }, },
+    -- Quick notes
+    {
+      "RutaTang/quicknote.nvim",
+      config = function()
+        -- you must call setup to let quicknote.nvim works correctly
+        require("quicknote").setup({})
+      end
+      ,
+      dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+      "epwalsh/obsidian.nvim",
+      version = "*", -- recommended, use latest release instead of latest commit
+      lazy = true,
+      -- ft = "markdown",
+      -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+      event = {
+        -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+        "BufReadPre " .. vim.fn.expand "~" .. "/knowledgebase/**.md",
+        "BufNewFile " .. vim.fn.expand "~" .. "/knowledgebase/**.md",
+      },
+      dependencies = {
+        -- Required.
+        "nvim-lua/plenary.nvim",
+
+      },
+      opts = {
+        workspaces = {
+          {
+            name = "knowledgebase",
+            path = "~/knowledgebase",
+          },
+
+        },
+
+      },
+    },
     -- Vim tabular plugin for manipulate tabular, required by markdown plugins
-    { "godlygeek/tabular",                cmd = { "Tabularize" }, },
+    { "godlygeek/tabular", cmd = { "Tabularize" }, },
 
   }
 )
