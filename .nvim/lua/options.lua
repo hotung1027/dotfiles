5aef484c8d34fbcffde7064ab1a643b476c46007
Randy TANG <hotung1027@gmail.com>
2024-11-05 09:03:10 +0800

    [fix] : bypass nofiles no handles by auto-session

--- .nvim/lua/options.lua
+++ .nvim/lua/options.lua
@@ -147,12 +147,13 @@ local sessions = vim.api.nvim_create_augroup('sessions', {})
 -- Auto save session
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   callback = function()
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-      -- Don't save while there's any 'nofile' buffer open.
-      if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
-        return
-      end
-    end
+    -- Escape nofile handle by auto-session
+    -- for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    --   -- Don't save while there's any 'nofile' buffer open.
+    --   if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
+    --     return
+    --   end
+    -- end
     vim.cmd [[SessionSave]]
   end
 })
